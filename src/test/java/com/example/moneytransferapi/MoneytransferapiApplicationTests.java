be748dae844de364281c7fc4740e90e8016d9b38
Amanuel2000 <amanuelf44@gmail.com>
2021-05-01 09:24:32 +0300

    initial commit - Spring Boot project created with dependencies

---
+++ src/test/java/com/example/moneytransferapi/MoneytransferapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moneytransferapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneytransferapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
